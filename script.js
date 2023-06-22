a726fbbb6065705477a5e35697e1e92169f33037
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-06-21 20:04:00 -0500

    cambios en layout

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-const JSON={
-    "Aqua":"200",
-    "Emoción":"180",
-    "Alegría":"160",
-    "Frescura":"150"
-};
+const form=document.getElementById("myForm");
+console.log(form);
+const infoForm=[];
 
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y la página se recargue
-  
-    // Obtén los valores de los campos del formulario
-    let juana = document.getElementById("perfumesJuana").value;
-    let pedro = document.getElementById("perfumesPedro").value;
-    let cantJuana=document.getElementById("cantJuana").value;
-    let cantPedro=document.getElementById("cantPedro").value;
-
-
-  
-    // Haz lo que necesites con los valores obtenidos
-    console.log("Perfume vendido por Juana:", juana," Cantidad vendida: ",cantJuana);
-    
-    console.log("Perfume vendido por Pedro:", pedro, " Cantidad vendida: ",cantPedro);
-  
-    // Puedes enviar los datos a un servidor, realizar validaciones, etc.
-    const totalVentJ=[];
-    const totalVentP=[];
-    if (juana==="Aqua") {
-        totalVentJ.push(200)
-    }else if (juana==="Emoción") {
-        totalVentJ.push(180);
-    }
-    console.log("total vendido por juana: ",totalVentJ)
-  
-    // Opcionalmente, puedes resetear el formulario después de obtener los datos
-    document.getElementById("myForm").reset();
-  });
-  
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target))
+    infoForm.push(data);
+    form.reset();
+    console.log(infoForm);
+})
